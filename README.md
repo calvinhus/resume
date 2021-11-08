887573b5b303d371df112ef99935baedfa960f47
calvinhus <gjpcalvinho@gmail.com>
2021-11-08 15:19:42 +0000

    Added files to deploy static webpage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# resume
+This repo has a simple static page with my personal info
+It is deployed in GitHub pages
---
+++ static/js/script.js
@@ -0,0 +1,44 @@
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+var swiper = new Swiper(".testimonialSwiper", {
+  navigation: {
+    nextEl: ".test-swiper-button-next",
+    prevEl: ".test-swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".certificatesSlider", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".cert-swiper-button-next",
+    prevEl: ".cert-swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+  },
+});
